4d90cba7c543032168b29891751ce1c73e6c022f
Konstntinos <kostaszaf@windowslive.com>
2022-10-18 12:55:32 +0300

    added a Dockerfile and created a docker image for the app

--- newsapi/provider/provider_api.py
+++ newsapi/provider/provider_api.py
@@ -18,8 +18,6 @@ def call_news_api(keyword: str) -> Tuple:
     response = requests.get(api_url, params=request_data)
 
     if response.status_code == 200:
-        print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
-        print(response.text)
         data = response.json()
         title = data["articles"][0]["title"]
         url = data["articles"][0]["url"]
